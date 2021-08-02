f7f27accc91b84d52071e6b09dbd4766632135c0
Justin Turner <jmturner638@gmail.com>
2021-08-02 10:05:14 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     #write your code below this line
+    while True:
+        phrase = input()
+
+        if phrase == '':
+            break
+
+        words = phrase.split()
+
+        for word in words:
+            print(word)
 
 if __name__ == '__main__':
     main()
